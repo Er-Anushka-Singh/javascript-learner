539a25d524a2a35db4b3cb506e7a242ebd7cc02c
Er-Anushka-Singh <anushkalucky11@gmail.com>
2025-09-01 06:40:08 +0530

    Add concept

---
+++ intro.js
@@ -0,0 +1,33 @@
+
+ console.log("Hello in javascript");
+
+
+//  Inline JavaScript 
+//   <button onclick="alert('hello')">Click me </button>
+
+// Internal JavaScript
+// <script> console.log('Hello, world! ); <script>
+
+// External JavaScript 
+// <script src="script.js"></script>
+
+// Value & Variables :-
+//                  A variable is a container(box) that holds a value.
+//                                            (Variable Name)
+//                                                     |
+//       (varisble key)  --->   var  myName  = "Vinod"  -->   (value)
+
+// value :-   A value is a piece of information that a program can work with .  It can be a number,text ,true/false, or more complex data.
+
+// variable:-   A variable is a container that holds a value . It has a name and can be used to store and manipulate data in a program.
+
+
+
+
+
+ var myAge = 30;
+ console.log(myAge);
+
+
+
+
